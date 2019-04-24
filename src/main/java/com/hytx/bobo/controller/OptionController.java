2e1a26b7349ed56da6ee71871544fed84f96a28c
cuipengbo <18003821657@163.com>
2019-04-24 11:51:49 +0800

    简单修改

--- src/main/java/com/hytx/bobo/controller/OptionController.java
+++ src/main/java/com/hytx/bobo/controller/OptionController.java
@@ -2,6 +2,8 @@ package com.hytx.bobo.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,9 @@ import com.hytx.bobo.service.OptionService;
 @RestController
 @RequestMapping("/option")
 public class OptionController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(OptionController.class);
+
 
 	@Autowired
     private OptionService optionService;
